c23b7d3f994199eb83bed9bb6a7712cc61d66851
ORMatheus <Matheus.rdeolivera77@gmail.com>
2026-01-14 21:09:43 -0300

    Issue3 finalizada

--- HistoryList.cpp
+++ HistoryList.cpp
@@ -1,5 +1,37 @@
 #include "HistoryList.h"
 
+void HistoryList::clearForward(){
+    Node* temp =current->next;
+
+    while(temp != nullptr){
+        Node* aux =temp->next;
+        delete temp;
+        temp=aux;
+    }
+    current->next=nullptr;
+    tail=current;   
+}
+
+void HistoryList::visit(const std::string& url){
+    if(head == nullptr){//caso a lista esjeta vazia 
+        Node* novoNo=new Node(url);
+        head=novoNo;
+        tail=novoNo;
+        current=novoNo;
+        return;
+    }
+    if(current != tail){ //caso o no current esteja em qualquer lugar que não seja o começo e o fim.
+        clearForward();
+    }
+
+    Node* novoNo=new Node(url);
+    tail->next=novoNo;
+    novoNo->prev=tail;
+    tail=novoNo;
+    current=novoNo;
+}
+
+
 Node::Node(const std::string& str){
     url=str;
     next=nullptr;
